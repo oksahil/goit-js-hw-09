257d758d537471daf8972ed9649510e342412146
oksahil <oksana.khylyk@gmail.com>
2022-11-22 22:22:46 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const throttle = require('lodash.throttle');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+}
+
+refs.startBtn.addEventListener('click', startBtnClick);
+refs.stopBtn.addEventListener('click', stopBtnClick);
+refs.stopBtn.setAttribute('disabled', true);
+// let colorClick = 0;
+function startBtnClick() {
+    startColorId = setInterval(() => {
+        const colorClick = getRandomHexColor();
+        refs.body.style.backgroundColor = colorClick;
+  }, 1000);
+  
+
+    
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled');
+    
+};
+
+
+function stopBtnClick() {
+    clearInterval(startColorId);
+    refs.stopBtn.setAttribute('disabled', true);
+    refs.startBtn.removeAttribute('disabled');
+};
